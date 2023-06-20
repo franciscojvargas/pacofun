a33b6881eaf9df5538b4e5012b63e8b7469807d1
Francisco Javier Vargas <frvagu@gmail.com>
2023-06-20 13:04:17 +0200

    modified:   inicio.js 	deleted:    serviceWorker.js

--- inicio.js
+++ inicio.js
@@ -3,12 +3,3 @@ $(document).ready(() => {
     window.location.href = "juego/index.html";
   });
 });
-
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function() {
-    navigator.serviceWorker
-      .register("/serviceWorker.js")
-      .then(res => console.log("service worker registered"))
-      .catch(err => console.log("service worker not registered", err))
-  })
-}
