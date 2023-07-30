555055a2c0d6d6c6ae9ebc172460a639a2a86154
franciscojvargas <frvagu@gmail.com>
2023-07-30 04:38:42 +0200

    modified:   juego/script.js 	modified:   juego/styles.css

--- juego/script.js
+++ juego/script.js
@@ -10,13 +10,12 @@ let currentRound = 1;
 let wordElement;
 let count = 30;
 
-// Cargar palabras
 const loadWords = async () => {
   try {
     const response = await fetch('palabras.txt');
     const fileContent = await response.text();
     const allWords = fileContent.split('\n').map(word => word.trim());
-    
+
     // Obtener 40 palabras aleatorias
     const randomIndices = [];
     while (randomIndices.length < 40) {
@@ -25,7 +24,7 @@ const loadWords = async () => {
         randomIndices.push(randomIndex);
       }
     }
-    
+
     words = randomIndices.map(index => allWords[index]);
     wordsCopy = [...words];
     console.log(wordsCopy);
@@ -284,19 +283,29 @@ const handleAnswer = (isCorrect) => {
   if (isCorrect) {
     // Sumar un punto al equipo del jugador actual
     if (currentPlayerIndex % 2 === 0) {
-      round1Score.red++;
-      console.log(`Puntuación equipo rojo: ${round1Score.red}`);
+      if (currentRound === 1) {
+        round1Score.red++;
+      } else if (currentRound === 2) {
+        round2Score.red++;
+      } else if (currentRound === 3) {
+        round3Score.red++;
+      }
     } else {
-      round1Score.blue++;
-      console.log(`Puntuación equipo azul: ${round1Score.blue}`);
+      if (currentRound === 1) {
+        round1Score.blue++;
+      } else if (currentRound === 2) {
+        round2Score.blue++;
+      } else if (currentRound === 3) {
+        round3Score.blue++;
+      }
     }
-    
+
     // Eliminar la palabra del array de palabras
     const wordIndex = words.indexOf(wordElement.textContent);
     if (wordIndex !== -1) {
       words.splice(wordIndex, 1);
     }
-    
+
     nextWord();
   } else {
     // Si es incorrecto, restar 4 segundos al contador y mostrar la siguiente palabra
@@ -329,15 +338,40 @@ const nextPlayerTurn = () => {
   nextPlayer();
 };
 
-const newRound = () => { // TODO
+/**
+ * newRound: Empieza una nueva ronda con nueva puntuación
+ */
+const newRound = () => {
   clearScreen();
 
-  // restablecer palabras
-  words = words.slice(0, words.length);
+  currentRound++;
 
-  // Actualizar currentRound
-  // Mostrar en pantalla el número de ronda actual y botón de continuar
-  // boton continuar, que al pulsarlo llame a nextPlayerTurn
+  words = wordsCopy.slice();
+
+  const container = document.createElement("div");
+  container.classList.add("container");
+  document.body.appendChild(container);
+
+  const roundNumberElement = document.createElement("h2");
+  roundNumberElement.textContent = `Ronda ${currentRound}`;
+  roundNumberElement.classList.add("round-number");
+  container.appendChild(roundNumberElement);
+
+  const continueButton = document.createElement("button");
+  continueButton.textContent = "Continuar";
+  continueButton.classList.add("btn", "btn-primary");
+  continueButton.addEventListener("click", () => {
+    if (currentRound > 3) {
+      showResults();
+    } else {
+      nextPlayer();
+    }
+  });
+  container.appendChild(continueButton);
+
+  if (currentRound > 3) {
+    showResults();
+  }
 };
 
 /**
@@ -375,6 +409,88 @@ const showResults = () => {
   blueHeaderCell.classList.add("blue");
   headerRow.appendChild(blueHeaderCell);
 
+  for (let i = 0; i < currentRound; i++) {
+    const roundRow = document.createElement("tr");
+    table.appendChild(roundRow);
+
+    const roundCell = document.createElement("td");
+    roundCell.textContent = `Ronda ${i + 1}`;
+    roundRow.appendChild(roundCell);
+
+    const redScoreCell = document.createElement("td");
+    redScoreCell.textContent =
+      i === 0 ? round1Score.red : i === 1 ? round2Score.red : round3Score.red;
+    redScoreCell.classList.add("red");
+    roundRow.appendChild(redScoreCell);
+
+    const blueScoreCell = document.createElement("td");
+    blueScoreCell.textContent =
+      i === 0 ? round1Score.blue : i === 1 ? round2Score.blue : round3Score.blue;
+    blueScoreCell.classList.add("blue");
+    roundRow.appendChild(blueScoreCell);
+  }
+
+  const separator = document.createElement("div");
+  separator.classList.add("my-3");
+  mainContent.appendChild(separator);
+
+  const buttonContainer = document.createElement("div");
+  buttonContainer.classList.add("d-flex", "justify-content-center");
+  mainContent.appendChild(buttonContainer);
+
+  const continueButton = document.createElement("button");
+  continueButton.textContent = currentRound === 3 ? "Terminar Juego" : "Continuar";
+  continueButton.classList.add("btn", "btn-primary");
+  continueButton.addEventListener("click", () => {
+    if (currentRound === 3) {
+      clearScreen();
+      showFinalResults();
+    } else {
+      newRound();
+    }
+  });
+  buttonContainer.appendChild(continueButton);
+};
+
+/**
+ * showFinalResults: muestra los resultados finales del juego.
+ */
+const showFinalResults = () => {
+  clearScreen();
+
+  const container = document.createElement("div");
+  container.classList.add("container");
+  document.body.appendChild(container);
+
+  const mainContent = document.createElement("div");
+  mainContent.classList.add("d-flex", "flex-column", "align-items-center");
+  container.appendChild(mainContent);
+
+  const finalScoresHeader = document.createElement("h2");
+  finalScoresHeader.textContent = "Resultados Finales";
+  mainContent.appendChild(finalScoresHeader);
+
+  const table = document.createElement("table");
+  table.classList.add("score-table");
+  mainContent.appendChild(table);
+
+  const headerRow = document.createElement("tr");
+  table.appendChild(headerRow);
+
+  const headerCell = document.createElement("th");
+  headerCell.textContent = "";
+  headerRow.appendChild(headerCell);
+
+  const redHeaderCell = document.createElement("th");
+  redHeaderCell.textContent = "Equipo Rojo";
+  redHeaderCell.classList.add("red");
+  headerRow.appendChild(redHeaderCell);
+
+  const blueHeaderCell = document.createElement("th");
+  blueHeaderCell.textContent = "Equipo Azul";
+  blueHeaderCell.classList.add("blue");
+  headerRow.appendChild(blueHeaderCell);
+
   for (let i = 0; i < 3; i++) {
     const roundRow = document.createElement("tr");
     table.appendChild(roundRow);
@@ -384,15 +500,37 @@ const showResults = () => {
     roundRow.appendChild(roundCell);
 
     const redScoreCell = document.createElement("td");
-    redScoreCell.textContent = round1Score.red;
+    redScoreCell.textContent =
+      i === 0 ? round1Score.red : i === 1 ? round2Score.red : round3Score.red;
     redScoreCell.classList.add("red");
     roundRow.appendChild(redScoreCell);
 
     const blueScoreCell = document.createElement("td");
-    blueScoreCell.textContent = round1Score.blue;
+    blueScoreCell.textContent =
+      i === 0 ? round1Score.blue : i === 1 ? round2Score.blue : round3Score.blue;
     blueScoreCell.classList.add("blue");
     roundRow.appendChild(blueScoreCell);
   }
+
+  const totalRedScore = round1Score.red + round2Score.red + round3Score.red;
+  const totalBlueScore = round1Score.blue + round2Score.blue + round3Score.blue;
+
+  const totalRow = document.createElement("tr");
+  table.appendChild(totalRow);
+
+  const totalCell = document.createElement("td");
+  totalCell.textContent = "Puntaje Total";
+  totalRow.appendChild(totalCell);
+
+  const totalRedScoreCell = document.createElement("td");
+  totalRedScoreCell.textContent = totalRedScore;
+  totalRedScoreCell.classList.add("red");
+  totalRow.appendChild(totalRedScoreCell);
+
+  const totalBlueScoreCell = document.createElement("td");
+  totalBlueScoreCell.textContent = totalBlueScore;
+  totalBlueScoreCell.classList.add("blue");
+  totalRow.appendChild(totalBlueScoreCell);
 
   const separator = document.createElement("div");
   separator.classList.add("my-3");
